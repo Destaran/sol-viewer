7b68ecd471772a6b67f89148051508471ae6c539
Destaran <abonyibalazs95@gmail.com>
2023-08-23 16:48:31 +0200

    separated planets data into new file

---
+++ src/data/planets.tsx
@@ -0,0 +1,82 @@
+export const planets = [
+  {
+    name: "Mercury",
+    scale: 0.0002439,
+    color: "#9d3102",
+    orbit: {
+      perihelion: 46.0,
+      aphelion: 69.818,
+      inclination: 7.004,
+    },
+  },
+  {
+    name: "Venus",
+    scale: 0.0006052,
+    color: "#f7ff86",
+    orbit: {
+      perihelion: 107.48,
+      aphelion: 108.941,
+      inclination: 3.395,
+    },
+  },
+  {
+    name: "Earth",
+    scale: 0.0006371,
+    color: "#1b5dc7",
+    orbit: {
+      perihelion: 147.1,
+      aphelion: 152.1,
+      inclination: 0,
+    },
+  },
+  {
+    name: "Mars",
+    scale: 0.0003389,
+    color: "#cd0000",
+    orbit: {
+      perihelion: 206.65,
+      aphelion: 249.261,
+      inclination: 0,
+    },
+  },
+  {
+    name: "Jupiter",
+    scale: 0.0069911,
+    color: "#63ffc3",
+    orbit: {
+      perihelion: 740.595,
+      aphelion: 816.363,
+      inclination: 1.304,
+    },
+  },
+  {
+    name: "Saturn",
+    scale: 0.0058232,
+    color: "#feffca",
+    orbit: {
+      perihelion: 1357.554,
+      aphelion: 1506.527,
+      inclination: 2.486,
+    },
+  },
+  {
+    name: "Uranus",
+    scale: 0.0025362,
+    color: "#740070",
+    orbit: {
+      perihelion: 2732.696,
+      aphelion: 3001.39,
+      inclination: 0.77,
+    },
+  },
+  {
+    name: "Neptune",
+    scale: 0.0024622,
+    color: "#83f5ff",
+    orbit: {
+      perihelion: 4471.05,
+      aphelion: 4558.857,
+      inclination: 1.77,
+    },
+  },
+];
