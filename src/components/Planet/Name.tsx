16d2e223dbb2984f7399497f3b600e785601c0d5
Destaran <abonyibalazs95@gmail.com>
2023-08-23 22:01:00 +0200

    implementing Planet Name fading - not working

--- src/components/Planet/Name.tsx
+++ src/components/Planet/Name.tsx
@@ -1,6 +1,6 @@
 import { Text } from "@react-three/drei";
 import { Vector3, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function Name({
   name,
@@ -12,37 +12,53 @@ export function Name({
   lookAtPlanet,
 }) {
   const nameRef = useRef();
-
+  const [show, setShow] = useState(false);
   const namePos: Vector3 = [
     position.x + 1.8,
     position.y + 1.8,
     position.z + 1.8,
   ];
 
+  // useEffect(() => {
+  //   if
+  // })
+
   useFrame(() => {
-    nameRef.current.rotation.x = camRef.current.rotation.x;
-    nameRef.current.rotation.y = camRef.current.rotation.y;
-    nameRef.current.rotation.z = camRef.current.rotation.z;
-    const distance = nameRef.current.position.distanceTo(
+    const distance = meshRef.current.position.distanceTo(
       camRef.current.position
     );
-    nameRef.current.fontSize = 0.03 * distance;
+    if (distance < scale * 100 && show) {
+      setShow(false);
+    } else if (distance > scale * 100 && !show) {
+      setShow(true);
+    }
+    if (show) {
+      nameRef.current.rotation.x = camRef.current.rotation.x;
+      nameRef.current.rotation.y = camRef.current.rotation.y;
+      nameRef.current.rotation.z = camRef.current.rotation.z;
 
-    nameRef.current.position.x = meshRef.current.position.x;
-    nameRef.current.position.y = meshRef.current.position.y + 1;
-    nameRef.current.position.z = meshRef.current.position.z;
+      nameRef.current.fontSize = 0.03 * distance;
+
+      nameRef.current.position.x = meshRef.current.position.x;
+      nameRef.current.position.y = meshRef.current.position.y + 1;
+      nameRef.current.position.z = meshRef.current.position.z;
+    }
   });
 
   return (
-    <Text
-      ref={nameRef}
-      anchorX="center"
-      anchorY="middle"
-      position={namePos}
-      color={color}
-      onClick={() => lookAtPlanet(meshRef.current.position, scale)}
-    >
-      {name}
-    </Text>
+    <>
+      {show ? (
+        <Text
+          ref={nameRef}
+          anchorX="center"
+          anchorY="middle"
+          position={namePos}
+          color={color}
+          onClick={() => lookAtPlanet(meshRef.current.position, scale)}
+        >
+          {name}
+        </Text>
+      ) : null}
+    </>
   );
 }
