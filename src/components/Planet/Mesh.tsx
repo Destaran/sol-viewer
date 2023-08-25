01476515e1573e0ed8efc78f2f54a6d2596a952f
Destaran <abonyibalazs95@gmail.com>
2023-08-25 02:40:09 +0200

    fixed rotation of orbits

--- src/components/Planet/Mesh.tsx
+++ src/components/Planet/Mesh.tsx
@@ -16,18 +16,17 @@ export function Mesh({ planet, meshRef, lookAtPlanet, track }: MeshProps) {
   const { nodes, materials } = useGLTF(`/src/glb/${name}.glb`);
   const haveRings = name === "Saturn";
 
-  if (track && track === name) {
+  if (track === name) {
     lookAtPlanet(meshRef.current.position, scale);
   }
 
-  const orbitTime = (60 * 60 * 24) / orbitPeriod;
   const semiMajorAxis = (perihelion + aphelion) / 2;
 
   useFrame(({ clock }) => {
     meshRef.current.rotation.y += 0.003;
-    const time = clock.getElapsedTime() + 12512521;
+    const time = -clock.getElapsedTime() / 60 / 60 / 24 + 12512521;
     const rotate = -Math.PI / 2;
-    const angle = (time / orbitTime) * 2 * Math.PI;
+    const angle = time * orbitPeriod * 2 * Math.PI;
     const radius =
       (semiMajorAxis * (1 - eccentricity * eccentricity)) /
       (1 + eccentricity * Math.cos(angle));
