2e8aa5a7bdc9bccc4ef9160f10f41a096cb1ec1e
Destaran <abonyibalazs95@gmail.com>
2023-09-05 14:55:48 +0200

    Moon tsx fix

--- src/components/Moon.tsx
+++ src/components/Moon.tsx
@@ -1,24 +1,28 @@
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { Group, Mesh } from "three";
 
-export function Moon(props) {
-  const { nodes, materials } = useGLTF("/Moon.glb");
-  const groupRef = useRef();
+export function Moon() {
+  const gltf = useGLTF(`/glb/Moon.glb`);
+  const mesh = gltf.scene.getObjectByName("Moon") as Mesh;
+  const groupRef = useRef<Group>(null);
   const radius = 12;
   useFrame(({ clock }) => {
+    if (!groupRef.current) {
+      return;
+    }
     const timer = clock.getElapsedTime() / 5;
-
     groupRef.current.position.x = 149.598 + Math.sin(timer) * radius;
     groupRef.current.position.z = Math.cos(timer) * radius;
   });
   return (
-    <group {...props} dispose={null} ref={groupRef}>
+    <group dispose={null} ref={groupRef}>
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Cube008.geometry}
-        material={materials["Default OBJ.005"]}
+        geometry={mesh.geometry}
+        material={mesh.material}
       />
     </group>
   );
